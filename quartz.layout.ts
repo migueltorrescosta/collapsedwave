3bab1212996f0272b3efa00e5db376dbf7aca69a
migueltorrescosta <miguelptcosta1995@gmail.com>
2023-11-17 20:16:45 +0000

    Improve Graph renderization

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {depth: 2, fontSize: 0.4},
+      globalGraph: {fontSize: 0.4}
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
