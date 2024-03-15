06a72e6e1b91e37fb92fdb83cd8cd8ecab1fd3bf
migueltorrescosta <miguelptcosta1995@gmail.com>
2024-03-15 17:40:10 +0100

    Improve 404 message

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -4,7 +4,7 @@ function NotFound() {
   return (
     <article class="popover-hint">
       <h1>404</h1>
-      I have yet to write this page. I'd be keen to hear how you would write up this page. Feel free to reach out directly, or <a href="https://github.com/migueltorrescosta/collapsedwave/issues/new/choose">submit a content request</a>.
+      I have yet to write this page. I would be keen to hear how you would write it, either by reaching out directly, or by <a href="https://github.com/migueltorrescosta/collapsedwave/issues/new/choose">submitting a content request on GitHub</a>.
     </article>
   )
 }
