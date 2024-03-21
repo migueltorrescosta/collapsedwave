3ed33930d1f89a50923ad68e7bf7962acc4c5c8f
migueltorrescosta <miguelptcosta1995@gmail.com>
2024-03-21 11:38:25 +0100

    Update 404 error message

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -5,7 +5,7 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   return (
     <article class="popover-hint">
       <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <p>I have yet to write this page. I would be keen to hear how you would write it, either by reaching out directly, or by <a href="https://github.com/migueltorrescosta/collapsedwave/issues/new/choose">submitting a content request on GitHub</a>.</p>
     </article>
   )
 }
